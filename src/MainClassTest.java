02e77a04d7d911ffb2546fe61b392fee68f3df8b
Nikita Myshkin <iggi1984@yandex.ru>
2023-07-10 16:33:47 +0300

    test2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,11 @@ public class MainClassTest extends MainClass
         System.out.println("getLocalNumber = " + this.getLocalNumber());
         Assert.assertTrue("Error! getLocalNumber is not 14!",this.getLocalNumber() == 14);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        System.out.println("getClassNumber = " + this.getClassNumber());
+        Assert.assertTrue("Error! getClassNumber cannot be less than 45!", this.getClassNumber() > 45);
+    }
 }
